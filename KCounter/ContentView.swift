73c1570ff5dd54aca4faa3ae83cb1fe97314a5c3
raddevus <1radeutsch1@gmail.com>
2024-05-29 16:39:10 -0400

    Moved watch app buttons away from each other and added ability to choose a background image from your photos.

--- KCounter/ContentView.swift
+++ KCounter/ContentView.swift
@@ -6,17 +6,43 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
     @State private var connectivity = Connector()
     
+    @State private var avatarItem: PhotosPickerItem?
+        @State private var avatarImage: Image?
+    
     var body: some View {
-        VStack(spacing: 30) {
-            Text("Game Counter")
-            Text(connectivity.receivedText)
+        ZStack{
+            avatarImage?
+            .resizable()
+            .scaledToFill()
+            .opacity(0.4)
+                        .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                Text("Game Counter")
+                Text(connectivity.receivedText)
+            }
+            .font(.title)
+            VStack {
+                PhotosPicker("Select background", selection: $avatarItem, matching: .images)
+                    .frame(maxHeight: .infinity, alignment: .bottom)
+            }
+            .onChange(of: avatarItem) {
+                Task {
+                    if let loaded = try? await avatarItem?.loadTransferable(type: Image.self) {
+                        avatarImage = loaded
+                        print(avatarImage)
+                    } else {
+                        print("Failed")
+                    }
+                }
+            }
         }
-        .font(.title)
     }
+    
 }
 
 #Preview {
